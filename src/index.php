3e8cbebb06303028c32940433eb1fb1134376a47
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2020-05-27 16:25:40 +0000

    Changing the background color to red and title
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>

--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple Sheldon Fargate PHP App</title>
+        <title>Simple Sheldon Whyte Fargate PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #b03043;}</style>
+        <style>body {margin-top: 40px; background-color: #FFFF00;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple Sheldon Fargate PHP App</h1>
+                <h1>Simple Sheldon Whyte Fargate PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
